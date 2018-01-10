cb2543b7fc7a5549c95aa94a1a112b46cf50dcce
BertoDiaz <heriberto.diazluis@gmail.com>
2018-01-10 10:35:29 +0100

    Add license

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,31 @@
-"""models.py."""
+"""
+File name: models.py
+
+Name: Inventory App
+
+Description: With this web application you can do the inventory of all
+             the material of your laboratory or business. You can also
+             place orders but this form is case-specific. Moreover,
+             you can track all your manufacturing procedures such as
+             wafer fabrication in this case.
+
+Copyright (C) 2017  Heriberto J. Díaz Luis-Ravelo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Email: heriberto.diazluis@gmail.com
+"""
 
 from django.db import models
 from django.utils import timezone
@@ -1118,7 +1145,7 @@ class Chip(models.Model):
     wafer = models.ForeignKey('Wafer', help_text="To what wafer is assigned.")
     # chip = models.IntegerField(help_text="ID of the chip.")
     chip = models.CharField(max_length=50, help_text="ID of the chip.")
-    date = models.DateField(blank=True, null=True, help_text="When was took.")
+    date = models.DateField(blank=True, null=True, help_text="MM/DD/YY")
     laser_source = models.CharField(max_length=50, blank=True,
                                     help_text="Source of light that is used.")
     readout = models.CharField(max_length=50, blank=True,
--- blog/views/waveguide.py
+++ blog/views/waveguide.py
@@ -1,4 +1,31 @@
-"""waveguide.py."""
+"""
+File name: waveguide.py
+
+Name: Inventory App
+
+Description: With this web application you can do the inventory of all
+             the material of your laboratory or business. You can also
+             place orders but this form is case-specific. Moreover,
+             you can track all your manufacturing procedures such as
+             wafer fabrication in this case.
+
+Copyright (C) 2017  Heriberto J. Díaz Luis-Ravelo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Email: heriberto.diazluis@gmail.com
+"""
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
